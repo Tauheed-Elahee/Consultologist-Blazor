a9b4c871d6526bdb31a0e760134d9d0e8a6bec9a
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-11-20 21:44:16 -0500

    Migrate from Bootstrap to Fluent UI. Fluent UI Icons are still missing not working.

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddMsalAuthentication(options =>
 {
