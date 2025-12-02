aceafa3e6207808e69269878c6761aa6a3babdc2
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-12-01 22:41:30 -0500

    Add CORS headers.

--- Api/AgentProxy.cs
+++ Api/AgentProxy.cs
@@ -30,6 +30,26 @@ public class AgentProxy
     [Function("AgentProxy")]
     public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", "options")] HttpRequest req)
     {
+        // Add CORS headers
+        var origin = req.Headers["Origin"].ToString();
+        var allowedOrigins = new[]
+        {
+            "https://app.consultologist.ai",
+            "https://gentle-desert-09697700f.3.azurestaticapps.net",
+            "http://localhost:3000",
+            "http://localhost:5000",
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:7071"
+        };
+
+        if (allowedOrigins.Contains(origin))
+        {
+            req.HttpContext.Response.Headers.Append("Access-Control-Allow-Origin", origin);
+            req.HttpContext.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            req.HttpContext.Response.Headers.Append("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        }
+
         // Handle CORS preflight
         if (req.Method == "OPTIONS")
         {
